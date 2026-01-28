1d527f85c9a39cc5163a05505a49062f4a2b32a6
rahul <rb6764385@gmail.com>
2026-01-28 11:58:15 +0530

    Add initial JavaScript files for conversion operations, comparisons, and data types summary

---
+++ 05_datatypes_summary.js
@@ -0,0 +1,16 @@
+//primitive
+
+
+//7 types: String,Number,Boolean, null,undefined,Symbol,BigInt
+
+const score =100;
+
+const scoreValue=100.3;
+
+
+
+
+
+//non primitive(Reference)
+
+// Array,Objects,Functions
